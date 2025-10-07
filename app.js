53b62ea575ca4e677ef4405c501799aec9f9b7d5
Nobody12here <uraja01313@gmail.com>
2025-10-07 16:08:40 +0500

    Refactor membership SMS message format to improve readability by removing redundant fields

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ async function sendSms(messageType, details) {
             throw new Error("Missing required fields for membership");
         }
 
-        message = `Membership Purchased\nEmail: ${email}\nOrder ID: ${id}\nQuantity: ${quantity}\nUSDT Amount: ${usdt_amount}\nCrypto: ${crypto_currency}\nReceiver Address: ${receiver_address}\nMembership Added: ${membership_added}\nPurchase Date: ${purchase_date}`;
+        message = `Membership Purchased\nEmail: ${email}\n\nQuantity: ${quantity}\nUSDT Amount: ${usdt_amount}\nCrypto: ${crypto_currency}\nReceiver Address: ${receiver_address}\n`;
     }
     else {
         throw new Error("Invalid message type");
